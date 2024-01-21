7dea41a3e28de6ae361468f6e04a0410492618c5
Nialods <105827271+Nialods@users.noreply.github.com>
2024-01-21 19:14:59 -0300

    refatorando o código par aimplementação de histórico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,7 @@ function Square({ value, onSquareClick }): JSX.Element {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array<string>(9).fill(""));
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i: number) {
     const nextSquares = squares.slice();
@@ -31,8 +29,7 @@ export default function Board() {
       nextSquares[i] = 'O';
     }
     
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -64,6 +61,29 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill("")]);
+
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares: Array<string>) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={ xIsNext } squares={ currentSquares } onPlay={ handlePlay } />
+      </div>
+      <div className='game-info'>
+        <ol>{ /*to do*/ }</ol>
+      </div>
+    </div>
   );
 }
 
