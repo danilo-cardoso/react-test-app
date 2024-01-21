7d987e34ecf37308645875511ead95d0e42a4e62
Nialods <105827271+Nialods@users.noreply.github.com>
2024-01-21 20:26:49 -0300

    adicionando funcionalidade de histórico e refatorando código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,38 +42,52 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? 'X' : 'O');
   }
 
+  const boardSquares = squares.map((square: string, index: number) => {
+      return (
+        <Square key={index} value={square} onSquareClick={() => handleClick(index)}/>
+      )
+  })
+
   return (
   <>
     <div className=' pb-2 font-semibold'>{ status }</div>
-    <div className="flex h-10">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="flex h-10">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="flex h-10">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    <div className=" grid-cols-3 grid-rows-3 inline-grid h-auto">
+      { boardSquares }
     </div>
   </>
   );
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill("")]);
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares: Array<string> = history[currentMove];
 
   function handlePlay(nextSquares: Array<string>) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+  
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+  
+  const moves = history.map((squares, move) => {
+    let description: string;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={ move }>
+        <button onClick={() => jumpTo(move)}>{ description }</button>
+      </li>
+    );
+  });
 
   return (
     <div className='game'>
@@ -81,7 +95,7 @@ export default function Game() {
         <Board xIsNext={ xIsNext } squares={ currentSquares } onPlay={ handlePlay } />
       </div>
       <div className='game-info'>
-        <ol>{ /*to do*/ }</ol>
+        <ol>{ moves }</ol>
       </div>
     </div>
   );
